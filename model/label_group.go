1e4258e10c1e77924303137904b4259bc99b3861
xiaosha <panxiao0811@gmail.com>
2020-11-22 19:03:51 +0800

    # 标签分组添加type

--- model/label_group.go
+++ model/label_group.go
@@ -7,6 +7,7 @@ type LabelGroup struct {
 	Status    int8    `json:"status"`
 	Color     string  `json:"color" gorm:"varchar(10)"`
 	Labels    []Label `json:"labels"`
+	Type      int8    `json:"type"` // 1 系统, 2 自定义
 	CreatedAt MyTime  `json:"created_at"`
 	UpdatedAt MyTime  `json:"updated_at"`
 }
