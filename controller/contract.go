6cc8ff113ad241df420f2c462e8423996fdef5a2
xiaosha <panxiao0811@gmail.com>
2020-11-28 20:50:59 +0800

    # newly 查询条件修改

--- controller/contract.go
+++ controller/contract.go
@@ -341,7 +341,7 @@ func GetContractByStatus(c *gin.Context) {
 		// db.Preload("Contracts", func(db *gorm.DB) *gorm.DB {
 		// 	return db.Order("created_at DESC").Limit(1)
 		// }).Find(&members)
-		db.Preload("Contracts").Where("first_creat < ?", time.Now().Add(7*24*time.Hour)).Find(&members)
+		db.Preload("Contracts").Where("first_create < ?", time.Now().Add(7*24*time.Hour)).Find(&members)
 	} else if r.Type == "inserve" { // 服务中客户
 		db.Preload("Contracts").Where("status = 1").
 			Where("expire_time > ?", time.Now().Add(-1*time.Hour*24*30).Format("2006-01-02")).
