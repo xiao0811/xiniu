c74cb8fc96afd39bb27bb2365b71d2dcf932c8f8
xiaosha <panxiao0811@gmail.com>
2020-12-02 21:38:10 +0800

    # 文件fmt

--- controller/message.go
+++ controller/message.go
@@ -7,16 +7,17 @@ import (
 	"github.com/xiao0811/xiniu/handle"
 )
 
+// SendMessage 发送信息
 func SendMessage(c *gin.Context) {
 	var r handle.Info
 	if err := c.ShouldBind(&r); err != nil {
 		handle.ReturnError(http.StatusBadRequest, "输入数据格式不正确", c)
 		return
 	}
-	if id, err := r.Send(); err != nil {
+	id, err := r.Send()
+	if err != nil {
 		handle.ReturnError(http.StatusBadRequest, "短信发送失败", c)
 		return
-	} else {
-		handle.ReturnSuccess("ok", id, c)
 	}
+	handle.ReturnSuccess("ok", id, c)
 }
